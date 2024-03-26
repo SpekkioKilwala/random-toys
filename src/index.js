dd014ee2aa761fe3a7f30cd478ecdd936e72b77f
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-03-26 21:11:04 +1100

    Add elo (and check it works)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
 import _ from 'lodash';
 import { testBOM} from './bom.js';
+import Elo from '@pelevesque/elo'
 
-function inputBox(_id) {
-	const element = document.createElement('div');
-	const txt = document.createElement('input');
-	txt.setAttribute('type', 'text');
-	txt.setAttribute('id', _id);
+// function inputBox(_id) {
+// 	const element = document.createElement('div');
+// 	const txt = document.createElement('input');
+// 	txt.setAttribute('type', 'text');
+// 	txt.setAttribute('id', _id);
 
-	element.appendChild(txt);
-	return element;
-}
+// 	element.appendChild(txt);
+// 	return element;
+// }
 
-function pareSequence(e) {
-	let input = e.target.value;
-	console.log("input: " + input);
+// function pareSequence(e) {
+// 	let input = e.target.value;
+// 	console.log("input: " + input);
 
-	const filter = /[^gcatGCAT ,.']/g;
+// 	const filter = /[^gcatGCAT ,.'?]/g;
 
-	let output = input.replaceAll(filter, "");
+// 	let GCAT = input.replaceAll(filter, "");
+// 	let singleSpaced = GCAT.replaceAll(/[ ]+/g, " ")
 
-	document.getElementById('sequence').value = output;
-}
+// 	document.getElementById('sequence').value = singleSpaced;
+// }
 
 
-document.body.appendChild(inputBox("rawtext"));
-document.body.appendChild(inputBox("sequence"));
+// document.body.appendChild(inputBox("rawtext"));
+// document.body.appendChild(inputBox("sequence"));
+
+const elo = new Elo();
+console.log(elo.getExpectedScore(2000, 1500));
 
 document.getElementById('rawtext').addEventListener('change', pareSequence);
 
